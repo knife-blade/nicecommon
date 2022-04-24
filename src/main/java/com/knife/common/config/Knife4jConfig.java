9eec2206df0dbc024c002f423e1efce7091affa1
water <2716552863@qq.com>
2022-04-24 21:37:26 +0800

    修改knife4j配置类

--- src/main/java/com/knife/common/config/Knife4jConfig.java
+++ src/main/java/com/knife/common/config/Knife4jConfig.java
@@ -1,35 +1,40 @@
 package com.knife.common.config;
- 
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
+// @EnableKnife4j
+// @EnableSwagger2
 public class Knife4jConfig {
-    @Bean(value = "defaultApi2")
-    public Docket defaultApi2() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+    @Bean
+    public Docket docket() {
+        Docket docket = new Docket(DocumentationType.OAS_30)
                 .apiInfo(new ApiInfoBuilder()
                         .title("我的标题")
                         .description("我的描述")
                         // .termsOfServiceUrl("http://www.xx.com/")
-                        .contact(new Contact("IT利刃出鞘", "https://knife.blog.csdn.net", "xx@qq.com"))
+                        .contact(new Contact("knife", "https://knife.blog.csdn.net/", "xx@qq.com"))
                         .version("1.0")
                         .build())
-                //分组名称
+                // 分组名称
                 .groupName("all")
                 .select()
-                //指定Controller扫描路径。可以不具体到controller，它会扫描指定路径下的所有
-                .apis(RequestHandlerSelectors.basePackage("com.knife"))
+                // 这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                 .paths(PathSelectors.any())
                 .build();
+
         return docket;
     }
 }
