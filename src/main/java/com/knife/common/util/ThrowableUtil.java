907032cda89fc08f819618e74e19c00cb42af302
knifeedge <2716552863@qq.com>
2022-06-15 09:02:10 +0800

    异常工具添加简略信息方法

--- src/main/java/com/knife/common/util/ThrowableUtil.java
+++ src/main/java/com/knife/common/util/ThrowableUtil.java
@@ -89,4 +89,80 @@ public class ThrowableUtil {
         }
         return s.toString();
     }
+
+    /**
+     * 获取最后n条简略堆栈信息（方法名加行号）
+     *
+     * @param e 异常对象
+     * @param n 最后n条
+     * @return 错误信息
+     */
+    public static String getBriefLastStackTrace(Throwable e, Integer n) {
+        Integer lineNumber = n;
+        if (lineNumber == null) {
+            lineNumber = 10;
+        }
+
+        StringBuilder s = new StringBuilder().append(e);
+        int i = 0;
+        for (StackTraceElement traceElement : e.getStackTrace()) {
+            String info = traceElement.getMethodName() + ":" + traceElement.getLineNumber();
+
+            s.append("    at ").append(info);
+            if (i >= lineNumber) {
+                break;
+            }
+            i++;
+        }
+        return s.toString();
+    }
+
+    /**
+     * 获取以指定包名为前缀的所有简略堆栈信息（方法名+包名）
+     *
+     * @param e             异常
+     * @param packagePrefix 包前缀
+     * @return 堆栈信息
+     */
+    public static String getBriefStackTraceByPackage(Throwable e, String packagePrefix) {
+        StringBuilder s = new StringBuilder().append(e);
+
+        for (StackTraceElement traceElement : e.getStackTrace()) {
+            // 如果是指定的包
+            if (traceElement.getClassName().startsWith(packagePrefix)) {
+                String info = traceElement.getMethodName() + ":" + traceElement.getLineNumber();
+                s.append("    at ").append(info);
+            }
+        }
+        return s.toString();
+    }
+
+    /**
+     * 获取以指定包名为前缀的最后n条简略堆栈信息（方法名+包名）
+     *
+     * @param e             异常
+     * @param packagePrefix 包前缀
+     * @return 堆栈信息
+     */
+    public static String getLastBriefStackTraceByPackage(Throwable e, String packagePrefix, Integer n) {
+        Integer lineNumber = n;
+        if (lineNumber == null) {
+            lineNumber = 10;
+        }
+
+        StringBuilder s = new StringBuilder().append(e);
+        int i = 0;
+        for (StackTraceElement traceElement : e.getStackTrace()) {
+            // 如果是指定的包
+            if (traceElement.getClassName().startsWith(packagePrefix)) {
+                String info = traceElement.getMethodName() + ":" + traceElement.getLineNumber();
+                s.append("    at ").append(info);
+            }
+            if (i >= lineNumber) {
+                break;
+            }
+            i++;
+        }
+        return s.toString();
+    }
 }
