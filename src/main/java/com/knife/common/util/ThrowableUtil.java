dd2da1b5edb3e6eb1aed2ed6bd3e6fd1eeb93da5
knifeedge <2716552863@qq.com>
2022-06-15 08:54:55 +0800

    异常工具添加方法

--- src/main/java/com/knife/common/util/ThrowableUtil.java
+++ src/main/java/com/knife/common/util/ThrowableUtil.java
@@ -7,7 +7,7 @@ public class ThrowableUtil {
     /**
      * 获取以指定包名为前缀的堆栈信息
      *
-     * @param e             异常
+     * @param e 异常
      * @return 堆栈信息
      */
     public static String getStackTrace(Throwable e) {
@@ -19,25 +19,8 @@ public class ThrowableUtil {
     }
 
     /**
-     * 获取以指定包名为前缀的堆栈信息
-     *
-     * @param e             异常
-     * @param packagePrefix 包前缀
-     * @return 堆栈信息
-     */
-    public static String getStackTraceByPackage(Throwable e, String packagePrefix) {
-        StringBuilder s = new StringBuilder().append(e);
-        for (StackTraceElement traceElement : e.getStackTrace()) {
-            if (!traceElement.getClassName().startsWith(packagePrefix)) {
-                break;
-            }
-            s.append("    at ").append(traceElement);
-        }
-        return s.toString();
-    }
-
-    /**
      * 获取最后n条堆栈信息
+     *
      * @param e 异常对象
      * @param n 最后n条
      * @return 错误信息
@@ -52,6 +35,53 @@ public class ThrowableUtil {
         int i = 0;
         for (StackTraceElement traceElement : e.getStackTrace()) {
             s.append("    at ").append(traceElement);
+            if (i >= lineNumber) {
+                break;
+            }
+            i++;
+        }
+        return s.toString();
+    }
+
+    /**
+     * 获取以指定包名为前缀的所有堆栈信息
+     *
+     * @param e             异常
+     * @param packagePrefix 包前缀
+     * @return 堆栈信息
+     */
+    public static String getStackTraceByPackage(Throwable e, String packagePrefix) {
+        StringBuilder s = new StringBuilder().append(e);
+
+        for (StackTraceElement traceElement : e.getStackTrace()) {
+            // 如果是指定的包
+            if (traceElement.getClassName().startsWith(packagePrefix)) {
+                s.append("    at ").append(traceElement);
+            }
+        }
+        return s.toString();
+    }
+
+    /**
+     * 获取以指定包名为前缀的最后n条堆栈信息
+     *
+     * @param e             异常
+     * @param packagePrefix 包前缀
+     * @return 堆栈信息
+     */
+    public static String getLastStackTraceByPackage(Throwable e, String packagePrefix, Integer n) {
+        Integer lineNumber = n;
+        if (lineNumber == null) {
+            lineNumber = 10;
+        }
+
+        StringBuilder s = new StringBuilder().append(e);
+        int i = 0;
+        for (StackTraceElement traceElement : e.getStackTrace()) {
+            // 如果是指定的包
+            if (traceElement.getClassName().startsWith(packagePrefix)) {
+                s.append("    at ").append(traceElement);
+            }
             if (i >= lineNumber) {
                 break;
             }
