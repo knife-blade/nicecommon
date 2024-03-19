6b3a360d043d6dc9a1624cf3d0b58c3f0b166eea
knifeout <2716552863@qq.com>
2024-03-19 15:40:39 +0800

    添加公共对象

---
+++ src/main/java/com/knife/example/common/core/entity/CommonEntity.java
@@ -0,0 +1,56 @@
+package com.knife.example.common.core.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 数据库公共实体类
+ */
+@Data
+public class CommonEntity {
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    /**
+     * 创建人ID
+     */
+    private String createId;
+
+    /**
+     * 创建人名字
+     */
+    private String createName;
+
+    /**
+     * 更新人ID
+     */
+    private String updateId;
+
+    /**
+     * 更新人名字
+     */
+    private String updateName;
+
+    /**
+     * 删除标记。0：未删除；其他：已删除
+     */
+    @TableLogic("id")
+    private Long deleteFlag;
+}
