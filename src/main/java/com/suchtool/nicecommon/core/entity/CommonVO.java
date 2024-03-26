65de8e8907e3ff34797881fefd06a708003b8066
knifeout <2716552863@qq.com>
2024-03-26 15:02:38 +0800

    添加公共VO

---
+++ src/main/java/com/suchtool/nicecommon/core/entity/CommonVO.java
@@ -0,0 +1,33 @@
+package com.suchtool.nicecommon.core.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 数据库公共实体类
+ */
+@Data
+public class CommonVO {
+    @ApiModelProperty("主键")
+    private Long id;
+
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty("修改时间")
+    private LocalDateTime updateTime;
+
+    @ApiModelProperty("创建人ID")
+    private String createId;
+
+    @ApiModelProperty("创建人名字")
+    private String createName;
+
+    @ApiModelProperty("更新人ID")
+    private String updateId;
+
+    @ApiModelProperty("更新人名字")
+    private String updateName;
+}
