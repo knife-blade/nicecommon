0ccc6422f06550bd10ae644a9efd25a65216cede
knifeout <2716552863@qq.com>
2024-03-28 09:43:44 +0800

    全局异常打印日志

--- src/main/java/com/suchtool/nicecommon/core/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/suchtool/nicecommon/core/advice/GlobalExceptionAdvice.java
@@ -4,6 +4,7 @@ import com.suchtool.nicecommon.core.entity.ResultWrapper;
 import com.suchtool.nicecommon.core.exception.AuthenticationException;
 import com.suchtool.nicecommon.core.exception.BusinessException;
 import com.suchtool.nicecommon.core.exception.SystemException;
+import com.suchtool.nicelog.util.log.NiceLogUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.Ordered;
@@ -31,6 +32,11 @@ public class GlobalExceptionAdvice implements Ordered {
 
     @ExceptionHandler(Exception.class)
     public ResultWrapper<?> handleException(Exception e) throws Exception {
+        NiceLogUtil.createBuilder()
+                .mark("异常")
+                .errorInfo(e.getMessage())
+                .throwable(e)
+                .error();
 
         // 若某个自定义异常有@ResponseStatus注解，就继续抛出，这样状态码就能正常响应
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
@@ -42,6 +48,12 @@ public class GlobalExceptionAdvice implements Ordered {
 
     @ExceptionHandler(NullPointerException.class)
     public ResultWrapper<?> handleNullPointerException(NullPointerException e) {
+        NiceLogUtil.createBuilder()
+                .mark("空指针异常")
+                .errorInfo(e.getMessage())
+                .throwable(e)
+                .error();
+
         return ResultWrapper.error().message("空指针异常，请联系客服");
     }
 
@@ -50,20 +62,38 @@ public class GlobalExceptionAdvice implements Ordered {
      */
     @ExceptionHandler(BindException.class)
     public ResultWrapper<?> handleBindException(BindException e) {
-        String message = e.getAllErrors().stream()
+        String errorInfo = e.getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(", "));
 
-        return ResultWrapper.error().message(message);
+        NiceLogUtil.createBuilder()
+                .mark("数据校验异常")
+                .errorInfo(errorInfo)
+                .throwable(e)
+                .error();
+
+        return ResultWrapper.error().message(errorInfo);
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResultWrapper<?> handleBusinessException(BusinessException e) {
+        NiceLogUtil.createBuilder()
+                .mark("业务异常")
+                .errorInfo(e.getMessage())
+                .throwable(e)
+                .error();
+
         return ResultWrapper.error().message(e.getMessage());
     }
 
     @ExceptionHandler(SystemException.class)
     public ResultWrapper<?> handleSystemException(SystemException e) {
+        NiceLogUtil.createBuilder()
+                .mark("系统异常")
+                .errorInfo(e.getMessage())
+                .throwable(e)
+                .error();
+
         return ResultWrapper.error().message("系统异常，请联系客服");
     }
 }
