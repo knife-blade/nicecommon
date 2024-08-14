ab123b547a0a569ba1b47c9b4ecbb7124243ab8b
knifeout <2716552863@qq.com>
2024-08-14 08:58:48 +0800

    1.0.10版本发布（优化全局响应处理）

--- src/main/java/com/suchtool/nicecommon/core/advice/GlobalResponseBodyAdvice.java
+++ src/main/java/com/suchtool/nicecommon/core/advice/GlobalResponseBodyAdvice.java
@@ -7,20 +7,19 @@ import com.suchtool.nicecommon.core.entity.ResultWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
 
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ordered {
     private final int order;
 
@@ -33,6 +32,7 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ord
         return order;
     }
 
+
     /**
      * 返回值的含义：是否要处理
      */
@@ -41,7 +41,6 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ord
                             Class<? extends HttpMessageConverter<?>> converterType) {
         // 若接口返回的类型本身就是ResultWrapper，则无需操作，返回false
         // return !methodParameter.getParameterType().equals(ResultWrapper.class);
-
         // 本处全部都放过，在后边进行处理
         return true;
     }
@@ -52,8 +51,6 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ord
                                   MediaType selectedContentType,
                                   Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                   ServerHttpRequest request, ServerHttpResponse response) {
-        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
         if (ProcessIgnoreUrl.isInWrapperIgnoreUrl(request.getURI().getPath())) {
             // 如果不需要处理，直接跳过
             return body;
@@ -63,9 +60,9 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ord
         if (!(executable instanceof Method)) {
             return body;
         }
+
         Method method = (Method) executable;
         Class<?> returnType = method.getReturnType();
-
         if (returnType == String.class) {
             // 若返回值为String类型，需要包装为String类型返回。否则会报错
             try {
@@ -79,7 +76,6 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object>, Ord
             // 如果已经封装过了，不再封装
             return body;
         }
-
         return ResultWrapper.success().data(body);
     }
 }
