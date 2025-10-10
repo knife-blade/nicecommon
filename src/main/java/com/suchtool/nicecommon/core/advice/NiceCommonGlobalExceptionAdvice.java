c73f2d10a213ee8234d849d60874be5440e63b51
knifeout <2716552863@qq.com>
2025-10-10 11:16:30 +0800

    1.1.46版本发布（优化异常处理）

--- src/main/java/com/suchtool/nicecommon/core/advice/NiceCommonGlobalExceptionAdvice.java
+++ src/main/java/com/suchtool/nicecommon/core/advice/NiceCommonGlobalExceptionAdvice.java
@@ -1,16 +1,18 @@
 package com.suchtool.nicecommon.core.advice;
 
 import com.suchtool.nicecommon.core.exception.BusinessException;
-import com.suchtool.nicecommon.core.exception.CustomCodeException;
+import com.suchtool.nicecommon.core.exception.CatchException;
 import com.suchtool.nicecommon.core.exception.CustomResultException;
 import com.suchtool.nicecommon.core.exception.SystemException;
 import com.suchtool.nicecommon.core.model.ResultWrapper;
 import com.suchtool.nicecommon.core.property.NiceCommonGlobalExceptionProperty;
 import com.suchtool.nicelog.util.log.NiceLogUtil;
+import com.suchtool.nicetool.util.base.ThrowableUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,33 +39,62 @@ public class NiceCommonGlobalExceptionAdvice implements Ordered {
 
     @ExceptionHandler(Exception.class)
     public ResultWrapper<?> handleException(Exception e) throws Exception {
-        if (enableGlobalExceptionAdviceLog) {
-            NiceLogUtil.createBuilder()
-                    .mark("异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
-                    .error();
-        }
+        String errorMessage = process(e, "异常");
 
         // 若某个自定义异常有@ResponseStatus注解，就继续抛出，这样状态码就能正常响应
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
             throw e;
         }
 
-        return ResultWrapper.error().message(e.getMessage());
+        return ResultWrapper.error().message(errorMessage);
+    }
+
+    @ExceptionHandler(Error.class)
+    public ResultWrapper<?> handleError(Error e)  {
+        String errorMessage = process(e, "错误");
+
+        // 若某个自定义异常有@ResponseStatus注解，就继续抛出，这样状态码就能正常响应
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        return ResultWrapper.error().message(errorMessage);
+    }
+
+
+    @ExceptionHandler(BusinessException.class)
+    public ResultWrapper<?> handleBusinessException(BusinessException e) {
+        String errorMessage = process(e, "业务异常");
+
+        return ResultWrapper.error().message(errorMessage);
+    }
+
+    @ExceptionHandler(CatchException.class)
+    public ResultWrapper<?> handleCatchException(CatchException e) {
+        String errorMessage = process(e, "捕获异常");
+
+        return ResultWrapper.error().message(errorMessage);
+    }
+
+    @ExceptionHandler(SystemException.class)
+    public ResultWrapper<?> handleSystemException(SystemException e) {
+        String errorMessage = process(e, "系统异常");
+
+        return ResultWrapper.error().message(errorMessage);
+    }
+
+    @ExceptionHandler(CustomResultException.class)
+    public ResultWrapper<?> handleCustomResultException(CustomResultException e) {
+        process(e, "自定义返回值异常");
+
+        return e.getResult();
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResultWrapper<?> handleNullPointerException(NullPointerException e) {
-        if (enableGlobalExceptionAdviceLog) {
-            NiceLogUtil.createBuilder()
-                    .mark("空指针异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
-                    .error();
-        }
+        process(e, "空指针异常");
 
-        return ResultWrapper.error().message("空指针异常，请联系客服");
+        return ResultWrapper.error().message("空指针异常");
     }
 
     /**
@@ -86,55 +117,28 @@ public class NiceCommonGlobalExceptionAdvice implements Ordered {
         return ResultWrapper.error().message(errorInfo);
     }
 
-    @ExceptionHandler(BusinessException.class)
-    public ResultWrapper<?> handleBusinessException(BusinessException e) {
+    private String process(Throwable throwable, String mark) {
+        String lastStackTraceString = null;
+        String errorMessage = throwable.getMessage();
+        Throwable causeThrowable = throwable;
+
+        if (throwable.getCause() != null) {
+            causeThrowable = throwable.getCause();
+            lastStackTraceString = ThrowableUtil.stackTraceToString(throwable);
+            if (!StringUtils.hasText(errorMessage)) {
+                errorMessage = causeThrowable.getMessage();
+            }
+        }
+
         if (enableGlobalExceptionAdviceLog) {
             NiceLogUtil.createBuilder()
-                    .mark("业务异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
+                    .mark(mark)
+                    .message(errorMessage)
+                    .throwable(causeThrowable)
+                    .errorInfo(lastStackTraceString)
                     .error();
         }
 
-        return ResultWrapper.error().message(e.getMessage());
-    }
-
-    @ExceptionHandler(SystemException.class)
-    public ResultWrapper<?> handleSystemException(SystemException e) {
-        if (enableGlobalExceptionAdviceLog) {
-            NiceLogUtil.createBuilder()
-                    .mark("系统异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
-                    .error();
-        }
-
-        return ResultWrapper.error().message("系统异常，请联系客服");
-    }
-
-    @ExceptionHandler(CustomCodeException.class)
-    public ResultWrapper<?> handleCustomCodeException(CustomCodeException e) {
-        if (enableGlobalExceptionAdviceLog) {
-            NiceLogUtil.createBuilder()
-                    .mark("自定义编码异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
-                    .error();
-        }
-
-        return ResultWrapper.success().code(e.getCode()).message(e.getMessage());
-    }
-
-    @ExceptionHandler(CustomResultException.class)
-    public ResultWrapper<?> handleCustomResultException(CustomResultException e) {
-        if (enableGlobalExceptionAdviceLog) {
-            NiceLogUtil.createBuilder()
-                    .mark("自定义返回值异常")
-                    .errorInfo(e.getMessage())
-                    .throwable(e)
-                    .error();
-        }
-
-        return e.getResult();
+        return errorMessage;
     }
 }
